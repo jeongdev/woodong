8c29fccec3f0881e8dbb13ad25e3fda6774912d8
jeongdev <jjeongvel@gmail.com>
2023-04-26 14:40:12 +0900

    feat: map moves when clicked

--- src/components/Map/InfoWindow.jsx
+++ src/components/Map/InfoWindow.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 
-export default function InfoWindow({ data }) {
+export default function InfoWindow({ data, moveMap }) {
   const [category, setCategory] = useState("전체");
   const location = [
     "전체",
@@ -12,7 +12,7 @@ export default function InfoWindow({ data }) {
     "대구",
     "울산",
     "부산",
-    "경상",
+    "경상도",
     "제주",
   ];
   const scrollRef = useRef();
@@ -24,6 +24,11 @@ export default function InfoWindow({ data }) {
   const cateChangeHandler = (e) => {
     setCategory(e.target.value);
     cateScroll();
+    moveMap(e.target.value);
+  };
+
+  const hospitalClickEvent = (address) => {
+    moveMap(address, "smooth");
   };
 
   return (
@@ -51,40 +56,42 @@ export default function InfoWindow({ data }) {
                 category === "전체" ? true : x.location === category
               )
               .map((item, idx) => (
-                <li key={idx} className="border-b border-gray-200 py-4">
-                  <a href="">
-                    <h3 className="text-lg font-semibold">
-                      {item.title}
-                      {item.reserve && (
-                        <span className="text-indigo-700 ml-1 text-sm">
-                          예약요망
-                        </span>
-                      )}
-                    </h3>
-                    <p className="mb-2">{item.address}</p>
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="black"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                        />
-                      </svg>
-                      <span className="ml-1">{item.phoneNumber}</span>
-                    </div>
-                    {item.memo && (
-                      <p className="mt-2 py-2 px-1 rounded-md bg-slate-200 text-sm">
-                        " {item.memo} "
-                      </p>
+                <li
+                  key={idx}
+                  className="border-b border-gray-200 py-4 cursor-pointer"
+                  onClick={() => hospitalClickEvent(item.address)}
+                >
+                  <h3 className="text-lg font-semibold">
+                    {item.title}
+                    {item.reserve && (
+                      <span className="text-indigo-700 ml-1 text-sm">
+                        예약요망
+                      </span>
                     )}
-                  </a>
+                  </h3>
+                  <p className="mb-2">{item.address}</p>
+                  <div className="flex items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="black"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                      />
+                    </svg>
+                    <span className="ml-1">{item.phoneNumber}</span>
+                  </div>
+                  {item.memo && (
+                    <p className="mt-2 py-2 px-1 rounded-md bg-slate-200 text-sm">
+                      " {item.memo} "
+                    </p>
+                  )}
                 </li>
               ))}
         </ul>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,11 +5,34 @@ import InfoWindow from "./InfoWindow";
 
 const { kakao } = window;
 export default function Map() {
+  const [kakaoMap, setKakaoMap] = useState(null);
   const [location, setLocation] = useState({
     lat: null,
     lng: null,
   });
   const [data, setData] = useState(null);
+
+  const geocoder = new kakao.maps.services.Geocoder();
+  const getLatLngFunc = (address) => {
+    // 주소-좌표 변환 객체를 생성합니다
+    return new Promise((resolve, reject) => {
+      geocoder.addressSearch(address, async (result, status) => {
+        // 정상적으로 검색이 완료됐으면
+        if (status !== kakao.maps.services.Status.OK) return;
+        const lat = result[0].y;
+        const lng = result[0].x;
+
+        resolve({ lat, lng });
+      });
+    });
+  };
+
+  const infoWindowClickHandler = async (address, option) => {
+    // 지도의 중심을 결과값으로 받은 위치로 즉시 이동시킵니다
+    const getCoords = await getLatLngFunc(address);
+    const coords = new kakao.maps.LatLng(getCoords.lat, getCoords.lng);
+    option === "smooth" ? kakaoMap.panTo(coords) : kakaoMap.setCenter(coords);
+  };
 
   const currentPosition = (position) => {
     const lat = position.coords.latitude;
@@ -33,7 +56,7 @@ export default function Map() {
         setData(data.data().list);
       });
   };
-  const initialMap = () => {
+  const initialMap = async () => {
     const mapContainer = document.getElementById("map"), // 지도를 표시할 div
       mapOption = {
         center: new kakao.maps.LatLng(location.lat, location.lng), // 지도의 중심좌표
@@ -42,6 +65,7 @@ export default function Map() {
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     const map = new kakao.maps.Map(mapContainer, mapOption);
+    if (!kakaoMap) setKakaoMap(map);
 
     // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
     var mapTypeControl = new kakao.maps.MapTypeControl();
@@ -54,7 +78,7 @@ export default function Map() {
     var zoomControl = new kakao.maps.ZoomControl();
     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     // 주소-좌표 변환 객체를 생성합니다
-    const geocoder = new kakao.maps.services.Geocoder();
+    // const geocoder = new kakao.maps.services.Geocoder();
 
     // 마커 이미지의 이미지 주소입니다
     // const imageSrc =
@@ -62,28 +86,25 @@ export default function Map() {
 
     // 주소로 좌표를 검색합니다\
     try {
-      data.map((item, idx) => {
-        geocoder.addressSearch(item.address, (result, status) => {
-          // 정상적으로 검색이 완료됐으면
-          if (status === kakao.maps.services.Status.OK) {
-            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            const marker = new kakao.maps.Marker({
-              map: map,
-              position: coords,
-            });
-
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            const infowindow = new kakao.maps.InfoWindow({
-              content: `<div style="width:150px;text-align:center;padding:6px 0;">${item.title}</div>`,
-            });
-            infowindow.open(map, marker);
-
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            // map.setCenter(coords);
-          }
+      data.map(async (item, idx) => {
+        const getCoords = await getLatLngFunc(item.address);
+        const coords = new kakao.maps.LatLng(getCoords.lat, getCoords.lng);
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position: coords,
         });
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        const infowindow = new kakao.maps.InfoWindow({
+          content: `<div style="width:150px;text-align:center;padding:6px 0;">
+              <a href="https://map.kakao.com/link/search/${item.title}
+              " target="_blank">
+              ${item.title}</a></div>`,
+        });
+        infowindow.open(map, marker);
+
+        // }
+        // });
       });
     } catch (e) {
       console.log("에러", e);
@@ -105,7 +126,7 @@ export default function Map() {
       style={{ width: "100%", height: "1000px" }}
       className="overflow-hidden relative"
     >
-      <InfoWindow data={data} />
+      <InfoWindow data={data} moveMap={infoWindowClickHandler} />
       <div id="map" className="w-full h-full" />
     </div>
   );
