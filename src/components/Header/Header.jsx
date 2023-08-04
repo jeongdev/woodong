b28f29991337d2e268e138c62fc1f651c6a1f0b9
JJ <2kkLqBMscTLhYbfs3Jxv>
2023-08-04 14:18:19 +0900

    feat: add mobile nav active link
    1. 모바일 네비게이션 active 추가, map 처리
    2. 현재 페이지 표시를 위해 수정, 한번에 관리하기 위해 map 으로 처리

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { Dialog, Popover } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
+const navList = [
+  {
+    txt: "특수동물 병원 검색",
+    path: "/",
+  },
+  {
+    txt: "제보",
+    path: "/report",
+  },
+];
+
 export default function Header() {
+  const location = useLocation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
@@ -33,24 +45,18 @@ export default function Header() {
           </button>
         </div>
         <Popover.Group className="hidden md:flex md:gap-x-12">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              (isActive && "text-primary") +
-              " text-sm leading-6 text-gray-900 hover:font-bold"
-            }
-          >
-            특수동물 병원 검색
-          </NavLink>
-          <NavLink
-            to="/report"
-            className={({ isActive }) =>
-              (isActive && "text-primary") +
-              " text-sm leading-6 text-gray-900 hover:font-bold"
-            }
-          >
-            제보
-          </NavLink>
+          {navList.map((item, i) => (
+            <NavLink
+              key={i}
+              to={item.path}
+              className={({ isActive }) =>
+                (isActive && "text-primary") +
+                " text-sm leading-6 text-gray-900 hover:font-bold"
+              }
+            >
+              {item.txt}
+            </NavLink>
+          ))}
         </Popover.Group>
         {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
@@ -66,9 +72,9 @@ export default function Header() {
       >
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <NavLink to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">우리동네 동물병원</span>
-            </a>
+            </NavLink>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -80,22 +86,25 @@ export default function Header() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              {navList.map((item, i) => (
+                <div
+                  key={`m-${i}`}
+                  onClick={() =>
+                    location.pathname !== item.path && setMobileMenuOpen(false)
+                  }
+                  className="space-y-2 py-6"
                 >
-                  특수동물 병원 검색
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="/report"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  제보
-                </a>
-              </div>
+                  <NavLink
+                    to={item.path}
+                    className={({ isActive }) =>
+                      (isActive && "text-primary") +
+                      " -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    }
+                  >
+                    {item.txt}
+                  </NavLink>
+                </div>
+              ))}
             </div>
           </div>
         </Dialog.Panel>
