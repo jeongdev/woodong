69797f5bd0445782c0ff4682be6993711138048a
JJ <2kkLqBMscTLhYbfs3Jxv>
2023-08-16 17:34:17 +0900

    style: add default btn props
    1. 버튼 props로 onClick 추가
    2. 기능 추가를 위함

--- src/components/Common/Button/Buttons.jsx
+++ src/components/Common/Button/Buttons.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export function DefaultButtons({ txt }) {
+export function DefaultButtons({ txt, onClick }) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primaryFocused focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       {txt}
@@ -11,10 +12,11 @@ export function DefaultButtons({ txt }) {
   );
 }
 
-export function CancelButtons({ txt }) {
+export function CancelButtons({ txt, onClick }) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="text-sm font-semibold leading-6 text-gray-900"
     >
       {txt}
